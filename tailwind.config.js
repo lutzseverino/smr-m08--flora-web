a5c4dd0ee801cb7884e858d7f7f0977271d96e73
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-02-12 02:50:07 +0100

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        fontFamily: {
+            'krona': ['Krona\\ One', 'sans'],
+            'exo': ['Exo', 'sabs']
+        },
         extend: {},
     },
     variants: {
