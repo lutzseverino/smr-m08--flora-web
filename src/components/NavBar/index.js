46dcd7acd0e15209bb0b800ff372c4437f1817bc
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-02-12 02:49:40 +0100

    Tweak Navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,20 +2,23 @@ import Logo from "../../images/flora-logo.svg";
 
 const Navbar = () => {
     return (
-        <nav className="fixed top-0 w-screen pt-4 pb-4 pl-32 pr-32
-                        flex flex-row justify-around items-center">
+        <nav className="w-screen pt-4 pb-4 pl-64 pr-64
+                        flex flex-row justify-between items-center">
+
             <div>
                 <a href="index.html"><img className="cursor-pointer" src={Logo} alt="Flora logo"/></a>
             </div>
+
             <div className="list-none 
                             flex flex-row gap-8">
                 <a href="contribute.html">Contribute</a>
                 <a href="support.html">Support</a>
                 <a href="team.html">Our team</a>
             </div>
-                <div>
-                    <a href="https://discord.com"><button className="">Add to Discord</button></a>
-                </div>
+
+            <div>
+                <a href="https://discord.com"><button className="">Add to Discord</button></a>
+            </div>
         </nav>
     );
 };
