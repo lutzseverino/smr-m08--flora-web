545aa4e63819c78a5f0c09efdab8074f8bb9daa2
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-02-12 02:24:45 +0100

    Start of tailwind switch
    Create project & folder structure
    Create Navbar component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/NavBar";
+
+function App() {
+    return ( 
+        <div className ="">
+            <header className="">
+                <Navbar></Navbar>
+            </header>
+        </div>
+    );
+}
+
+export default App;
