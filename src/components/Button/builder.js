93cdfe2b5a88c9c152c30db59c80ffa1abcd47c9
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-02-13 23:45:38 +0100

    Add and use CLB library Create button builder.

---
+++ src/components/Button/builder.js
@@ -0,0 +1,27 @@
+import clb from 'clb'
+
+const buttonBuilder = clb({
+  base: 'font-exo rounded-full p-2 pl-6 pr-6 shadow-inner-component dark:shadow-inner-dark-component',
+  defaultVariants: {
+    type: 'default',
+    disabled: false,
+  },
+  variants: {
+    type: {
+        accent: 'text-white bg-blue dark:bg-green dark:text-black',
+        default: 'text-black bg-gray-200 dark:bg-gray-700 dark:text-gray-300',
+    },
+    disabled: {
+        true: 'cursor-not-allowed opacity-70',
+        false: 'transition-transform hover:scale-105 active:scale-95',
+    },
+  },
+  compoundVariants: [
+    // { type: 'default', disabled: true, classes: '' },
+    // { type: 'accent', disabled: true, classes: '' },
+    // { type: 'default', disabled: false, classes: '' },
+    // { type: 'accent', disabled: false, classes: '' },
+  ],
+})
+
+export default buttonBuilder;
