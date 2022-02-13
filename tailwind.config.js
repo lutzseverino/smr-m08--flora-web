d873aec22b1e0a3fba2ce0bf3e520a6ae5192062
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-02-13 23:48:59 +0100

    Configure tailwind Add default tag styles. Extend color palette and shadows.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ module.exports = {
             'krona': ['Krona\\ One', 'sans'],
             'exo': ['Exo', 'sabs']
         },
-        extend: {},
+        extend: {
+            colors: {
+                blue: '#5865F2',
+                green: '#57F287',
+                yellow: '#FEE75C',
+                pink: '#EB459E',
+                red: '#ED4245',
+            },
+            boxShadow: {
+                'inner-component': 'inset 0 1px 0 0 rgb(0 0 0/.05)',
+                'inner-dark-component': 'inset 0 1px 0 0 rgb(255 255 255/.05)',
+            },
+        },
     },
     variants: {
         extend: {},
