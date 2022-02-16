fb3bb1b9e05d2cc5ffe67c74e0381861977e0bbd
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-02-16 02:40:10 +0100

    Add markdown to html ability

--- src/Main.js
+++ src/Main.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
+import Contribute from './pages/Contribute';
 
 const Main = () => {
   return (
     <Router>
         <Routes> {/* The Switch decides which component to show based on the current URL.*/}
             <Route exact path='/' element={<Home/>}></Route>
+            <Route exact path='/contribute' element={<Contribute/>}></Route>
         </Routes>
     </Router>
   );
