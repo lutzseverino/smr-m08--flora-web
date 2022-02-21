8553aeafea576f097bfde17a37f54925d63c0e2c
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-02-21 14:41:02 +0100

    Create auto markdown article component

--- src/pages/Contribute.jsx
+++ src/pages/Contribute.jsx
@@ -1,38 +1,9 @@
-import { getMarkdownFromURL } from "../components/Markdown/parser"
-import { parseMarkdown } from "../components/Markdown/parser"
-import ReactMarkdown from "react-markdown";
+import MdArticle from "../components/MdArticle";
 
-const markdown = 
-`
-# Contributing to Flora
-###### Right now we don't have a Code of Conduct or issue/PR templates, please follow common sense when talking to the community and notifying about problems, thanks. 
-## Want to contribute financially?
-First of all, thanks! You're awesome, you're the one that keeps this ship going, same goes for every open source project out there.
-- Become a [patreon](https://patreon.com/jasperls)
-- Buy me a [ko-fi](https://ko-fi.com/jasperls)
-## Want to contribute with your knowledge?
-Awesome! Your work makes this project reach more people, but before committing a contribution, it's a good practice to create an issue in the [issues section](https://github.com/JasperEdits/Flora/issues) or search if there's already one that is already being taken care of.
-### Don't see an issue?
-Then your change hasn't been proposed, create an issue, we'll see if the change is needed and at what priority, please check your issue is related to Flora and not to any of our [dependencies](https://github.com/JasperEdits/Flora/blob/master/pom.xml).
-### Ready to make a change?
-Exciting! Now it's time to fork the repository and start committing your own changes, you can do this via GitHub Desktop, CLI or using built-in features in an IDE.
-- Make sure not to code meant for debugging or testing.
-### PR
-When you're done, open a pull request or PR.
-- Self-check your PR.
-- Flora developers will check your request.
-- Make sure you're in touch with us in the process of making the PR, as we may have some questions.
-### Merge time
-If everything goes smoothly, your PR will get merged! Congratulations.
-###### This document was inspired by [GitHub Docs's CONTRUBUTING.md](https://github.com/github/docs/blob/main/CONTRIBUTING.md).
-`
+export default class Contribute extends MdArticle {
 
-function Contribute() {
-    return (
-        <div className="max-w-[70ch] p-8 md:pt-32 md:pb-32 ml-auto mr-auto">
-            <ReactMarkdown>{markdown}</ReactMarkdown>
-        </div>
-    )
+    constructor(props) {
+        super(props, "https://raw.githubusercontent.com/frequential/flora/master/CONTRIBUTING.md");
+    }
 }
 
-export default Contribute;
---
+++ src/pages/Privacy.jsx
@@ -0,0 +1,9 @@
+import MdArticle from "../components/MdArticle";
+
+export default class Privacy extends MdArticle {
+
+    constructor(props) {
+        super(props, "https://raw.githubusercontent.com/frequential/flora/master/PRIVACY.md");
+    }
+}
+
