8d1b4405ed56473cedb5b4ea762bba38c5df274c
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-02-21 14:41:10 +0100

    Add privacy page

--- src/Main.js
+++ src/Main.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Contribute from './pages/Contribute';
+import Privacy from './pages/Privacy';
 
 const Main = () => {
   return (
     <Router>
         <Routes> {/* The Switch decides which component to show based on the current URL.*/}
-            <Route exact path='/' element={<Home/>}></Route>
-            <Route exact path='/contribute' element={<Contribute/>}></Route>
+            <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/contribute' element={<Contribute />}></Route>
+            <Route exact path='/privacy' element={<Privacy />}></Route>
         </Routes>
     </Router>
   );
