b6692b3cc3cbc7305212070c5b3d67f49ed67c92
Jasper Lutz Severino <jasperlutzseverino@gmail.com>
2022-03-17 02:26:44 +0100

    Cookies notification now disappears on accept

--- src/components/Cookies/index.jsx
+++ src/components/Cookies/index.jsx
@@ -4,18 +4,29 @@ import { useTranslation } from 'react-i18next';
 
 const Cookies = () => {
     const { t } = useTranslation();
-    return (
-        <div id="cookies-notification" className="fixed bottom-0 w-full h-16 p-4
-                        dark:bg-white bg-black
-                        flex flex-row justify-between items-center">
-            <div>
-                <p className="invert">{t('cookies.explenation')}</p>
+
+    if (localStorage.cookies !== "true") {
+        return (
+            <div id="cookies-notification" className="fixed bottom-0 w-full h-16 p-4
+                            dark:bg-white bg-black
+                            flex flex-row justify-between items-center">
+                <div>
+                    <p className="invert">{t('cookies.explenation')}</p>
+                </div>
+                <div>
+                    <button onClick={() => accept()} className={buttonClasses({type: 'accent'})}>{t('commons.accept')}</button>
+                </div>
+                
             </div>
-            <div>
-                <button onClick={() => document.querySelector("#cookies-notification").classList.toggle("hidden")} className={buttonClasses({type: 'accent'})}>{t('commons.accept')}</button>
-            </div>
-            
-        </div>
-    );
+        );
+    } else {
+        return (null);
+    }
 };
+
+const accept = () => {
+    localStorage.cookies = "true";
+    document.querySelector("#cookies-notification").classList.toggle("hidden");
+}
+
 export default Cookies;
